a27e482fb7c268d4b36145aa74cedac8ca7ed450
SebanSunny <sebansunny@cvglobal.co>
2019-03-08 10:17:01 +0530

    updated the Hello World project

---
+++ Hello World/Hello World/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Hello World
+//
+//  Created by Seban on 08/03/19.
+//  Copyright © 2019 Seban. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+  @autoreleasepool {
+      // insert code here...
+      NSLog(@"Hello, World!");
+  }
+  return 0;
+}
